f811e37649b8329f838ce7cecf8560ad30d8d474
hascoeri <hascoetraphael-29@yahoo.fr>
2018-02-12 16:11:28 +0100

    Ajout du travail sur la page statique de modification d'image

--- js/script.js
+++ js/script.js
@@ -1,56 +1,9 @@
-/* Javascript permettant un défilement avec une durée */
+$(function () {
+    var images = ["photo.jpeg", "photo2.jpg", "photo3.jpg", "photo4.jpg", "photo5.jpg", "photo6.jpg", "photo7.jpg", "photo8.jpg"];
 
+    var start = 0;
 
-
-$(document).ready(function() {
-                
-                
-                $('#bloc_centre').css("height",$('body').height()-$('.pied-de-page').height());
-                
-                    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
-                    var page = $(this).attr('href'); // Page cible
-                    var speed = 750; // Durée de l'animation (en ms)
-                    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-                    return false;
-                });
-
-                $('#ajoutPhoto').click(function () {
-                    affHTML("ajoute.html");
-                });
-
-                $('#supprimerPhoto').click( function() {
-                    affHTML("supprimer.html");
-                });
-
-                $('#share').click( function() {
-                    affHTML("share.html");
-                });
-
-
-                
-                $('.img_doss').click(function() {
-                    console.log('trqsds');
-                   $(this) .css("background-color","#ffb7b7");
-                });
-                    
-                function toggleAff() {
-                    if($('.canvas').css('display') == 'block') {
-                       
-                        $('.canvas').css('display','none');
-                        $('.canvas_container').text("");
-                    }
-                    else {
-                        $('.canvas').css('display','block');
-                    }
-                }
-
-                function affHTML(url) {
-                    toggleAff();
-                    $('.canvas_container').load(url, function() {
-                        $("#quit").click(function() {
-                            toggleAff();
-                        });
-                    });
-                }
-});    
-                
+    $('.photo').each(function(index) {
+        $(' <img src="images\\' + images[index] + '"> ').appendTo(this);
+    });
+});
