0b3ad21584b6c1433df5cc1fa3db1b57692b8d28
hascoeri <hascoetraphael-29@yahoo.fr>
2018-02-13 22:55:23 +0100

    Tests sur le drag-n-drop, re

--- js/script.js
+++ js/script.js
@@ -1,56 +1,110 @@
 /* Javascript permettant un défilement avec une durée */
 
 
+$(document).ready(function () {
 
-$(document).ready(function() {
-                
-                
-                $('#bloc_centre').css("height",$('body').height()-$('.pied-de-page').height());
-                
-                    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
-                    var page = $(this).attr('href'); // Page cible
-                    var speed = 750; // Durée de l'animation (en ms)
-                    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-                    return false;
+    $('html').on('dragover', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $('html').on('drop', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+
+    $('#bloc_centre').css("height", $('body').height() - $('.pied-de-page').height());
+    $('.js-scrollTo').on('click', function () { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate({scrollTop: $(page).offset().top}, speed); // Go
+        return false;
+    });
+
+    $('#ajoutPhoto').click(function () {
+        affHTML("ajoute.html");
+    });
+
+    $('#supprimerPhoto').click(function () {
+        affHTML("supprimer.html");
+    });
+
+    $('#share').click(function () {
+        affHTML("share.html");
+    });
+
+
+    $('.img_doss').click(function () {
+        console.log('trqsds');
+        $(this).css("background-color", "#ffb7b7");
+    });
+
+    function toggleAff() {
+        if ($('.canvas').css('display') == 'block') {
+
+            $('.canvas').css('display', 'none');
+            $('.canvas_container').text("");
+        }
+        else {
+            $('.canvas').css('display', 'block');
+        }
+    }
+
+    function affHTML(url) {
+        toggleAff();
+        $('.canvas_container').load("canvas/" + url, function () {
+            if(url === "ajoute.html"){
+                $('.upload-area').on('dragenter', function (e) {
+                    e.stopPropagation();
+                    e.preventDefault();
                 });
 
-                $('#ajoutPhoto').click(function () {
-                    affHTML("ajoute.html");
+                $('.upload-area').on('dragover', function (e) {
+                    e.stopPropagation();
+                    e.preventDefault();
                 });
 
-                $('#supprimerPhoto').click( function() {
-                    affHTML("supprimer.html");
+                $('.upload-area').on('drop', function (e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+
+                    var file = e.originalEvent.dataTransfer.files;
+                    var fd = new FormData();
+
+                    fd.append('file', file[0]);
+
+                    uploadData(fd);
                 });
 
-                $('#share').click( function() {
-                    affHTML("share.html");
-                });
+            }
+            $("#quit").click(function () {
+                toggleAff();
+            });
+        });
+    }
+});
 
+function uploadData(formdata) {
+    $.ajax({
+        url: 'php/upload.php',
+        type: 'post',
+        data: formdata,
+        contentType: false,
+        processData: false,
+        dataType: 'json',
+        success: function (response) {
+            alert("Success\n" + response);
+            show(response);
+        },
 
-                
-                $('.img_doss').click(function() {
-                    console.log('trqsds');
-                   $(this) .css("background-color","#ffb7b7");
-                });
-                    
-                function toggleAff() {
-                    if($('.canvas').css('display') == 'block') {
-                       
-                        $('.canvas').css('display','none');
-                        $('.canvas_container').text("");
-                    }
-                    else {
-                        $('.canvas').css('display','block');
-                    }
-                }
+        error: function (context, text, error) {
+            alert("Failure\n" + context.responseText + "\n" + text + "\n" + error);
+        }
+    });
+}
 
-                function affHTML(url) {
-                    toggleAff();
-                    $('.canvas_container').load("canvas/"+url, function() {
-                        $("#quit").click(function() {
-                            toggleAff();
-                        });
-                    });
-                }
-});    
+function show(img) {
+    //$("#image_upload").append()
+}
                 
--- php/upload.php
+++ php/upload.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Raph
- * Date: 13/02/2018
- * Time: 21:22
- */
+
+/* Getting file name */
+$filename = $_FILES['file']['name'];
+
+/* Getting File size */
+$filesize = $_FILES['file']['size'];
+
+/* Location */
+$location = "../upload/" . $filename;
+
+$return_arr = array();
+
+/* Upload file */
+if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
+    $src = "default.png";
+
+// checking file is image or not
+    if (is_array(getimagesize($location))) {
+        $src = $location;
+    }
+    $return_arr = array("name" => $filename, "size" => $filesize, "src" => $src);
+}
+
+echo print_r($return_arr);
