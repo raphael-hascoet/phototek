0cf55c52533798e9781698361b3b21d88a464f72
hascoeri <hascoetraphael-29@yahoo.fr>
2018-03-14 21:10:07 +0100

    Ajout du javascript de la partie modification

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 $(document).ready(function () {
     rmTmp();
 
-    affPage("dossiers.html");
+    affPage("modif.html");
 
     $('html').on('dragover', function (e) {
         e.preventDefault();
@@ -61,7 +61,59 @@ $(document).ready(function () {
 
             }
             if (url === "modif.html") {
+                var images = ["photo.jpeg", "photo2.jpg", "photo3.jpg", "photo4.jpg", "photo5.jpg", "photo6.jpg", "photo7.jpg", "photo8.jpg"];
 
+                var start = 0;
+
+                $('.photo').each(function(index) {
+                    $(' <img src="images\\' + images[index] + '"> ').appendTo(this);
+                });
+
+                $('#arrow-right').click(function () {
+                    if(start + 5 < images.length) {
+                        start++;
+                        $('.photo img').remove();
+                        $('.photo').each(function (index) {
+                            $(' <img src="images\\' + images[index+start] + '"> ').appendTo(this);
+                        });
+                    }
+                });
+
+                $('#arrow-left').click(function () {
+                    if(start > 0) {
+                        start--;
+                        $('.photo img').remove();
+                        $('.photo').each(function (index) {
+                            $(' <img src="images\\' + images[index+start] + '"> ').appendTo(this);
+                        });
+                    }
+                });
+
+                $('.photo').click(function (e) {
+                    if(e.target.src != undefined) {
+                        var images = $('#apercu, #original');
+                        images.find('img').remove();
+                        $(' <img src="' + e.target.src + '"> ').prependTo(images);
+                    }
+                });
+
+                $('#contrasteSlider').on("input", function () {
+                    $('#contrasteVal').val(this.value);
+                });
+
+                $('#contrasteVal').on("keyup", function () {
+                    if(!isNaN(this.value)){
+                        var val;
+                        if(+this.value < 0){
+                            val = 0;
+                        } else if(+this.value >= 100){
+                            val = 100;
+                        } else {
+                            val = +this.value;
+                        }
+                        $('#contrasteSlider').val(val);
+                    }
+                });
             }
         });
     }
