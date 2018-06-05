1a121f9bb8414e6827dfd6748aa991e07a83d0a2
hascoeri <hascoetraphael-29@yahoo.fr>
2018-06-05 17:01:39 +0100

    Suppression du master

--- php/rmtmp.php
+++
@@ -1,7 +0,0 @@
-<?php
-$files = glob('../tmp/*');
-foreach ($files as $file) {
-    if (is_file($file)) {
-        unlink($file);
-    }
-}
--- php/rmtmpimage.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$pathTmp = $_SERVER['DOCUMENT_ROOT'] . '/phototek/tmp/tmpimage.jpg';
-
-unlink($pathTmp);
